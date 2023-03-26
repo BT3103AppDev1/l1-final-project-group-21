feac710f4aa008563d47a5dbf2a633646e097278
tungkei <e0774490@u.nus.edu>
2023-03-26 12:03:52 +0800

    Added back files that got removed accidentally during pull req

--- project-budgety/src/router/index.js
+++ project-budgety/src/router/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import LoginPage from "@/views/LoginPage.vue";
 import RegistrationPage from "@/views/RegistrationPage.vue";
 import ResetPasswordPage from "@/views/ResetPasswordPage.vue";
+import DashboardPage from "@/views/DashboardPage.vue";
+import AnalyticsPage from "@/views/AnalyticsPage.vue";
+import BudgetPage from "@/views/BudgetPage.vue";
+import ProfilePage from "@/views/ProfilePage.vue";
 
 const routes = [
 	{
@@ -18,6 +22,26 @@ const routes = [
 		path: "/resetpassword",
 		name: "ResetPasswordPage",
 		component: ResetPasswordPage,
+	},
+	{
+		path: "/dashboard",
+		name: "Dashboard",
+		component: DashboardPage,
+	},
+	{
+		path: "/analytics",
+		name: "Analytics",
+		component: AnalyticsPage,
+	},
+	{
+		path: "/budget",
+		name: "Budget",
+		component: BudgetPage,
+	},
+	{
+		path: "/profile",
+		name: "Profile",
+		component: ProfilePage,
 	},
 ];
 
---
+++ project-budgety/src/views/BudgetPage.vue
@@ -0,0 +1,12 @@
+<!-- Add Budget Code here -->
+<template>
+	<h1>This is the Budget Page</h1>
+</template>
+
+<script>
+export default {
+	name: "Budget",
+};
+</script>
+
+<style></style>

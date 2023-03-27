2eab6506fbb5382061621ec8038f21d30089a77f
tungkei <e0774490@u.nus.edu>
2023-03-27 16:32:14 +0800

    changed base.css preferred color scheme to light

--- project-budgety/src/main.js
+++ project-budgety/src/main.js
@@ -6,15 +6,16 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 
 import router from "./router/index.js";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-import Vue3EasyDataTable from 'vue3-easy-data-table';
-import 'vue3-easy-data-table/dist/style.css';
-
+import Vue3EasyDataTable from "vue3-easy-data-table";
+import "vue3-easy-data-table/dist/style.css";
 
 library.add(fas);
 
 createApp(App)
-.component('EasyDataTable', Vue3EasyDataTable)
-.component("fa", FontAwesomeIcon).use(router).mount("#app");
+	.component("EasyDataTable", Vue3EasyDataTable)
+	.component("fa", FontAwesomeIcon)
+	.use(router)
+	.mount("#app");
 // createApp(App).mount("#app");

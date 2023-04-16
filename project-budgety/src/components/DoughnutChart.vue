4508c062d5d05f8c50d9dd8dab2232802d91aa51
tungkei <e0774490@u.nus.edu>
2023-04-16 14:03:10 +0800

    updated doughnut chart dates aft pull req

--- project-budgety/src/components/DoughnutChart.vue
+++ project-budgety/src/components/DoughnutChart.vue
@@ -1,7 +1,7 @@
 <template>
-    <Doughnut v-if="loaded" :data="chartData" :options="chartOptions" />
+	<Doughnut v-if="loaded" :data="chartData" :options="chartOptions" />
 </template>
-  
+
 <script>
 import { authentication } from "../firebase.js";
 import firebaseApp from "../firebase.js";
@@ -16,16 +16,16 @@ import {
 	getDocs,
 	getFirestore,
 } from "firebase/firestore";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'vue-chartjs';
-  
-ChartJS.register(ArcElement, Tooltip, Legend)
-  
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "vue-chartjs";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 const db = getFirestore(firebaseApp);
 
 export default {
-    name: "DoughnutChart",
-    components: { Doughnut },
+	name: "DoughnutChart",
+	components: { Doughnut },
 
 	async mounted() {
 		console.log("In DoughnutChart vue");
@@ -39,20 +39,19 @@ export default {
 		}
 	},
 
-    data() {
+	data() {
 		return {
-			loaded:false,
+			loaded: false,
 			categoryColours: {
-				"Fashion": '#856dc8',
-				"Entertainment": '#eb8ad0',
-				"Food": '#4f94bc',
-				"Transportation": '#cc8a4a',
-				"Healthcare": '#539f37',
-				"Groceries": '#ac986b',
-				"Rental": '#38aca5',
-				"Utilities": '#8e451c',
-				"Others": "#8f8f8f",
-
+				Fashion: "#856dc8",
+				Entertainment: "#eb8ad0",
+				Food: "#4f94bc",
+				Transportation: "#cc8a4a",
+				Healthcare: "#539f37",
+				Groceries: "#ac986b",
+				Rental: "#38aca5",
+				Utilities: "#8e451c",
+				Others: "#8f8f8f",
 			},
 			catLabels: [],
 			catColours: [],
@@ -64,7 +63,6 @@ export default {
 						label: "Total Amount",
 						backgroundColor: [],
 						data: [],
-						
 					},
 				],
 			},
@@ -74,16 +72,13 @@ export default {
 					legend: {
 						display: false,
 						// position: 'right'
-					}
+					},
 				},
-				elements: {
-	
-				},
+				elements: {},
 				responsive: true,
 				// aspectRatio: 2,
 				// maintainAspectRatio: true
-				
-			}
+			},
 		};
 	},
 
@@ -91,12 +86,14 @@ export default {
 		async getTop3Cats(userEmail) {
 			const today = new Date();
 			// First day is the day of the month - the day of the week
-			const firstDay = today.getDate() - today.getDay(); 
-			// Last day is first day + 6
-			const lastDay = firstDay + 6;
+			const firstDay = today.getDate() - today.getDay();
+			// Last day is first day + 7
+			const lastDay = firstDay + 7;
 
 			const weekStart = new Date(today.setDate(firstDay));
+			const formattedFullStartDate = this.formatDate(weekStart, "start");
 			const weekEnd = new Date(today.setDate(lastDay));
+			const formattedFullEndDate = this.formatDate(weekEnd, "end");
 
 			// Set beginning of week by changing date and time
 			const amtsRef = collection(db, "users", userEmail, "expenses");
@@ -104,14 +101,14 @@ export default {
 			// Filter from beginning of the month to current time
 			const q = query(
 				amtsRef,
-				where("Date", ">=", weekStart),
-				where("Date", "<=", new Date())
+				where("Date", ">=", formattedFullStartDate),
+				where("Date", "<=", formattedFullEndDate)
 			);
 			const amtsSnapshot = await getDocs(q);
 
 			// Store categories as {"Food": 0, "Fashion": 35, ...}
 			let allCatDict = {};
-        	amtsSnapshot.forEach((doc) => {
+			amtsSnapshot.forEach((doc) => {
 				let data = doc.data();
 				let expAmt = parseFloat(data.Amount);
 				let expCat = data.Category;
@@ -124,9 +121,9 @@ export default {
 				}
 			});
 
-			// Emit event to parent DashboardPage for Top 3 Categories section 
-			this.$emit('sendCatDict', allCatDict);
-			
+			// Emit event to parent DashboardPage for Top 3 Categories section
+			this.$emit("sendCatDict", allCatDict);
+
 			// Iterate through each category present and add to ChartJS data
 			let allCatList = Object.entries(allCatDict);
 			allCatList.forEach((cat) => {
@@ -140,8 +137,25 @@ export default {
 			this.chartData.labels = this.catLabels;
 			this.chartData.datasets[0].backgroundColor = this.catColours;
 			this.chartData.datasets[0].data = this.catData;
-		}		
-	}
-}
+		},
+		formatDate(dateObj, str) {
+			const currYear = dateObj.getFullYear();
+			const currMonth = dateObj.getMonth() + 1; // Month is zero-based so need to add 1
+			const formatMonth = currMonth.toString().padStart(2, "0");
+			const currDay = dateObj.getDate();
+			const formatDay = currDay.toString().padStart(2, "0");
+			let formattedFullDate;
+			if (str == "start") {
+				formattedFullDate = new Date(
+					`${currYear}-${formatMonth}-${formatDay}T00:00:00`
+				);
+			} else {
+				formattedFullDate = new Date(
+					`${currYear}-${formatMonth}-${formatDay}T23:59:59.99`
+				);
+			}
+			return formattedFullDate;
+		},
+	},
+};
 </script>
-  

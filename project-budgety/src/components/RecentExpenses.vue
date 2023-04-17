475f749c21a2d9be11c329322e561a9ac4da804f
evewlim <e0774590@u.nus.edu>
2023-04-17 16:29:09 +0800

    update expense

--- project-budgety/src/components/RecentExpenses.vue
+++ project-budgety/src/components/RecentExpenses.vue
@@ -72,7 +72,7 @@
 								<button
 									id="saveButton"
 									type="button"
-									v-on:click="saveData(data)"
+									v-on:click="updateData(data)"
 									class="btn btn-white"
 									data-dismiss="modal"
 								>
@@ -274,7 +274,7 @@ export default {
 			const formattedFullDate = `${currYear}-${formatMonth}-${formatDay}T${formatHours}:${formatMins}`;
 			this.currTime = formattedFullDate;
 		},
-		async saveData(allData) {
+		async updateData(allData) {
 			if (
 				this.item1 == "" ||
 				this.date1 == "" ||
